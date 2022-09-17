bbe4d57967a5da80338acf92c1a95f70d645bd42
unknown <mislam181266@bscse.uiu.ac.bd>
2022-09-17 14:12:03 +0600

    Backend Connected To Frontend

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,25 +5,33 @@ import {
     getUser,
     getUsers,
   } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 
 
 const router = express.Router();
-router.get("/checkauthentication", verifyToken, (req,res,next)=>{
-  res.send("hello user, you are logged in")
-})
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//   res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+//   res.send("hello user, you are logged in")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//   res.send("hello admin, you are logged in and you can delete all accounts")
+// })
 
 //UPDATE
-router.put("/:id",  updateUser);
+router.put("/:id", verifyUser,  updateUser);
 
 //DELETE
-router.delete("/:id",  deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //GET
-router.get("/:id",  getUser);
+router.get("/:id", verifyUser, getUser);
 
 //GET ALL
-router.get("/",  getUsers);
+router.get("/",  verifyAdmin, getUsers);
 
 export default router
